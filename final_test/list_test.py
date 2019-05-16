97096aba39e7f97c8c91ce04c7f2a1726e5354b6
Ankit Khandelwal <amtron521@gmail.com>
2019-05-16 18:30:27 -0400

    yeeticus

--- final_test/list_test.py
+++ final_test/list_test.py
@@ -1,13 +1,9 @@
 import smbus
 import time
-from datetime import datetime
-import struct
-import subprocess
+
 
 bus = smbus.SMBus(1)
-time.sleep(1)
 address = 0x08
-
 
 def main():
     while True:
@@ -21,44 +17,25 @@ def main():
         else:
             print("Try again.")
         time.sleep(1)
-
-
+    
 def send_command():
-    to_send = 1 # struct.pack("hhl", 1, 2, 3)
+    to_send = [1,2]
     # command = 1
     # to_send = command.encode("utf-8")
-    count = 0
-    before_send = datetime.now()
-    while True:
-	flag = 0
-	while flag == 0:
-	    try:
-       		bus.write_byte(address, to_send)
-        	flag = 1
-	    except:
-	        subprocess.call(['i2cdetect', '-y', '1'])
-	        flag = 0
-	count += 1
-        # now = datetime.now()
-        # if (now - before_send).total_seconds() >= 1.0:
-        #     break
-	print "\t" + count
-	if count >= 500:
-	    break
-	
-    now = datetime.now()
-    diff = (now - before_send).total_seconds() 
-    print "Byte rate: " + str(count/(diff)/8) + " bits/sec"
-
+    bus.write_i2c_block_data(address, 2, to_send)
     # call method on intermediate pi and wait for a response @max
-
 
 def send_request():
     to_send = 2
     bus.write_byte(address, to_send)
-    bus.read_byte(address)
+    time.sleep(3)
+    val_received = bus.read_byte(address)
+  #  while(val_received == None):
+  #      val_received = bus.read_byte_data(address)
+    ASCII_to_char = chr(val_received)
+    print(ASCII_to_char)
     # call method on intermediate pi and wait for a response @max
-
 
 if __name__ == "__main__":
     main()
+    
